a5845d66b4845142765dc5b14531d72a64668e66
Dimitri De Franciscis <dimitri@megadix.it>
2014-09-15 19:51:12 +0200

    cluster configuration

--- src/main/java/monitoring/RiakConfiguration.java
+++ src/main/java/monitoring/RiakConfiguration.java
@@ -3,37 +3,61 @@ package monitoring;
 import com.basho.riak.client.IRiakClient;
 import com.basho.riak.client.RiakException;
 import com.basho.riak.client.RiakFactory;
+import com.basho.riak.client.raw.pbc.PBClientConfig;
+import com.basho.riak.client.raw.pbc.PBClusterConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @ConfigurationProperties(prefix = "monitorin.riak")
 public class RiakConfiguration {
-    private String pbcHost;
-    private int pbcPort;
+    private List<String> pbcHosts = new ArrayList<>();
+    private List<Integer> pbcPorts = new ArrayList<>();
 
     public RiakConfiguration() {
     }
 
-    public String getPbcHost() {
-        return pbcHost;
+    public List<String> getPbcHosts() {
+        return pbcHosts;
     }
 
-    public void setPbcHost(String pbcHost) {
-        this.pbcHost = pbcHost;
+    public void setPbcHosts(List<String> pbcHosts) {
+        this.pbcHosts = pbcHosts;
     }
 
-    public int getPbcPort() {
-        return pbcPort;
+    public List<Integer> getPbcPorts() {
+        return pbcPorts;
     }
 
-    public void setPbcPort(int pbcPort) {
-        this.pbcPort = pbcPort;
+    public void setPbcPorts(List<Integer> pbcPorts) {
+        this.pbcPorts = pbcPorts;
     }
 
     @Bean
     public IRiakClient pbcClient() throws RiakException {
-        return RiakFactory.pbcClient(pbcHost, pbcPort);
+        PBClusterConfig clusterConfig = new PBClusterConfig(200);
+        // used to retrieve default values
+        PBClientConfig defaultClientConfig = PBClientConfig.defaults();
+
+        for (int i = 0; i < pbcHosts.size(); i++) {
+            PBClientConfig clientConfig = new PBClientConfig.Builder()
+                    .withSocketBufferSizeKb(defaultClientConfig.getSocketBufferSizeKb())
+                    .withHost(pbcHosts.get(i))
+                    .withPort(pbcPorts.get(i))
+                    .withPoolSize(defaultClientConfig.getPoolSize())
+                    .withInitialPoolSize(defaultClientConfig.getInitialPoolSize())
+                    .withIdleConnectionTTLMillis(defaultClientConfig.getIdleConnectionTTLMillis())
+                    .withConnectionTimeoutMillis(defaultClientConfig.getConnectionWaitTimeoutMillis())
+                    .withRequestTimeoutMillis(defaultClientConfig.getRequestTimeoutMillis())
+                    .build();
+
+                    clusterConfig.addClient(clientConfig);
+        }
+
+        return RiakFactory.newClient(clusterConfig);
     }
 }
